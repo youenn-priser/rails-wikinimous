0f7aa0da5a18bd89867ce13a02dfd12f911cee19
youenn priser <youenn.priser@gmail.com>
2020-02-25 14:56:53 +0100

    Populated DB with 11 instances of articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  new_article = Article.new(title: Faker::Beer.name.to_s, content: "#{Faker::Beer.style}, #{Faker::Beer.malts}, #{Faker::Beer.alcohol}")
+  new_article.save
+end
